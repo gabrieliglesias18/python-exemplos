b999a775ea20b1eaba483a16b73f3d2fcbb4ed8f
gabrieliglesias18 <gabriel.iglesias.freitas@gmail.com>
2024-04-23 16:39:17 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,13 @@
+nome="Gabriel Iglesias de Freitas"
+idade=16
+email='gabriel.iglesias.freitas@gmail.com'
+telefone='17 982305557'
+nota=8,99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
+
