9fbece19357e345171b55a5036c50b01737ee9cc
gabrieliglesias18 <gabriel.iglesias.freitas@gmail.com>
2024-04-30 16:33:24 -0300

    Criação input.py

---
+++ input.py
@@ -0,0 +1,10 @@
+# Entradas de Dados
+aluno = input('Digite o nome do aluno: ')
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
+falta = int(input('Digiter a quantidade de falta: '))
+media = (nota1+nota2+nota3)/3
+print('Aluno: ' +aluno)
+print('Média: ' + str(media))
+print('Falta(s): ' + str(falta))              
