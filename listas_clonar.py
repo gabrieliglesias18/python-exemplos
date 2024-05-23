50a21011d6b3bbd37ca14c4cb1585505c23cbe17
gabrieliglesias18 <gabriel.iglesias.freitas@gmail.com>
2024-05-23 10:12:37 -0300

    criaçãolista_clonar.py

---
+++ listas_clonar.py
@@ -0,0 +1,6 @@
+alunos = ['João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana']
+copia = list(alunos)
+print(f"Lista Original: {alunos}")
+print(f"Lista Cópia: {copia}")
+alunos.remove('Maria')
+print(f"Lista alterada: {alunos}")
