1b40063c206a02a9f84cc742b6f6d05049b7c149
gabrieliglesias18 <gabriel.iglesias.freitas@gmail.com>
2024-05-23 10:42:07 -0300

    Criação listas_comprimento.py

--- listas_remover.py
+++ listas_remover.py
@@ -8,6 +8,5 @@ alunos.pop(2)
 print(f"Lista de alunos: {alunos}")
 # Função del
 del alunos[3]
-print(f"Lista de alunos: {alunos}")
-# Método clear()
-print(f"Lista de alunos: {alunos}")
+print(f"Lista de alunos: {alunos}")print(f"Lista de alunos: {alunos}")
+# Método remove()
